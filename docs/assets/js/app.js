bf45a4344598c27e9262faa9e54481ade6294b4e
davidmirza <davemirza9@gmail.com>
2024-01-10 17:52:28 +0700

    update js

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {myFunction()};
+//window.onscroll = function() {myFunction()};
 
 // function myFunction() {
 //   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -6,25 +6,6 @@ window.onscroll = function() {myFunction()};
 //   var scrolled = (winScroll / height) * 100;
 //   document.getElementById("myBar").style.width = scrolled + "%";
 // }
-var typed = new Typed('#typed', {
-  strings: ["Software Developer","Rest API","FrontEnd","Mobile Apps","Web Based","Desktop Based"],
-  typeSpeed: 50,
-  backSpeed: 20,
-  loop: true
-});
-const observer  = new IntersectionObserver((entries) =>{
-  entries.forEach((entry) =>{
-    console.log(entry);
-    if(entry.isIntersecting){
-      entry.target.classList.add('show');
-    }
-    else{
-      entry.target.classList.remove('show');
-    }
-  })
-})
-const hiddenElement = document.querySelectorAll('.hidden');
-hiddenElement.forEach((el) => observer.observe(el));
 let delayed = false;
 function openMenu() {
 const icon = document.querySelector(".togle-menu");  
@@ -38,3 +19,45 @@ setTimeout(() => {
   console.log("1 sec")
 }, (delayed)?100:10);
 }
+var typed = new Typed('#typed', {
+  strings: ["Software Developer","Rest API","FrontEnd","Mobile Apps","Web Based","Desktop Based"],
+  typeSpeed: 50,
+  backSpeed: 20,
+  loop: true
+});
+
+// const observer  = new IntersectionObserver((entries) =>{
+//   entries.forEach((entry) =>{
+//     if(entry.isIntersecting){
+//       entry.target.classList.add('show');
+//     }
+//     else{
+//       entry.target.classList.add('hidden');
+//     }
+//   })
+// })
+
+
+let section = document.querySelectorAll('section');
+window.onscroll = () => {
+  section.forEach(sc => {
+    let top = window.scrollY +10;
+    let offset = sc.offsetTop - 15;
+    let height = sc.offsetHeight +150;
+    if(top >=offset && top < offset+height){
+      sc.classList.add('show');
+    }
+    else{
+      sc.classList.remove('show');
+    }
+  })
+}
+
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach((el) => observer.observe(el));
+
+
+function getScroll(){
+  console.log('scroll')
+}
+
